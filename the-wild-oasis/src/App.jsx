8f60c6da4be619e1729c9c5779000d2233473b8c
vlsubramaniam <subramaniam.vls@gmail.com>
2024-06-08 13:16:49 +0530

    -8-JUN-2024

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
 import GlobalStyles from './styles/GlobalStyles';
 import {
   Dashboard,
@@ -43,6 +44,26 @@ function App() {
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position='top-center'
+        gutter={12}
+        containerStyle={{ margin: '8px' }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 3000,
+          },
+          style: {
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: '16px 24px',
+            backgroundColor: 'var(--color-gray-0)',
+            color: 'var(--color-gray-700)',
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
--- the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import { formatCurrency } from '../../utils/helpers';
 import { deleteCabin } from '../../services/apiCabins';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
 
 const TableRow = styled.div`
   display: grid;
@@ -57,11 +58,12 @@ function CabinRow({ cabin }) {
   const { isPending, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
+      toast.success('Cabin successfully deleted!');
       queryClient.invalidateQueries({
         queryKey: ['cabin'],
       });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (
